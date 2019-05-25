59c42f1a7659c54d892ec708e3585f6d88deb2f0
Полиныч <matkunova@gmail.com>
2019-05-25 23:22:58 +0300

    класс RW, который лента, и немного тестов

--- MTtools.py
+++ MTtools.py
@@ -1,25 +1,87 @@
+#!/usr/bin/env python3
+
 def parseCmd(line, lineNo=0):
+    """ Из строки программы получает внутренее представление команды
+
+    >>> print(parseCmd('q1,1->q2,0,L'))
+    ('q1', '1', 'q2', '0', 'L')
+    """
     s = line.split('->')
     q0,c0 = s[0].split(',')
     q1,c1,M = s[1].split(',')
     return q0,c0,q1,c1,M
 
 def parse(text):
+    """ Из текста программы получает внутреннее представление программы
+
+    >>> import pprint
+    >>> P = '''q1,1->q2,0,L
+    ... q2,0->q2,0,L
+    ... q1,0->q3,1,R
+    ... q2,1->q3,0,L
+    ... q3,1->q4,0,N
+    ... q4,0->q2,0,R
+    ... q4,1->q0,0,N'''
+    >>> program = parse(P)
+    >>> pprint.pprint(program)
+    {('q1', '0'): ('q3', '1', 'R'),
+     ('q1', '1'): ('q2', '0', 'L'),
+     ('q2', '0'): ('q2', '0', 'L'),
+     ('q2', '1'): ('q3', '0', 'L'),
+     ('q3', '1'): ('q4', '0', 'N'),
+     ('q4', '0'): ('q2', '0', 'R'),
+     ('q4', '1'): ('q0', '0', 'N')}
+    """
+    
     prog = {}
     for n,l in enumerate(text.split('\n')):
         q0,c0,q1,c1,M = parseCmd(l,n+1)
         prog[q0,c0] = q1,c1,M
-    return prog    
+    return prog
 
-P = """q1,1->q2,0,L
-q2,0->q2,0,L
-q1,0->q3,1,R
-q2,1->q3,0,L
-q3,1->q4,0,N
-q4,0->q2,0,R
-q4,1->q0,0,N"""
-print(parse(P))
-print(parseCmd('q1,1->q2,0,L'))
+class RW:
+    """Считывающее устройство Машины Тьюринга
 
-
+    >>> print(RW())
+    _
+    """
+    band = ''
+    nulsb = '_'
+    pos = 0
     
+    def __init__(self, nulsb = '_'):
+        self.nulsb = nulsb
+        self.band = nulsb
+
+    def getsb(self):
+        return self.band[self.pos]
+
+    def setword(self, word):
+        self.band = word
+
+    def setsb(self, sb):
+        self.band = self.band[:self.pos]+sb+self.band[self.pos+1:]
+
+    def __str__(self):
+        return self.band
+
+    def move(self, direction):
+        if direction == 'L':
+            if self.pos > 0:
+                self.pos -= 1
+            else:
+                self.band = self.nulsb + self.band
+        elif direction == 'R':
+            self.pos += 1
+            if self.pos >= len(self.band)-1:
+                self.band = self.band + self.nulsb
+        if self.pos > 0 and self.band[0] == self.nulsb:
+            self.pos, self.band = self.pos-1, self.band[1:]
+        if self.pos < len(self.band)-1 and self.band[-1] == self.nulsb:
+            self.band = self.band[:-1]
+        
+        
+
+
+
+
